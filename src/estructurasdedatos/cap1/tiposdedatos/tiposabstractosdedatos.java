9e4a08695b7aa8770ab6142dc8ecb8a2294d0ae8
jeycofff1011 <30888461+jeycofff1011@users.noreply.github.com>
2017-08-28 21:29:02 -0500

    Create tiposabstractosdedatos.java

---
+++ src/estructurasdedatos/cap1/tiposdedatos/tiposabstractosdedatos.java
@@ -0,0 +1,18 @@
+packages estructurasdedatos.cap1.tiposdedatos.tiposabstractosdedatos.java
+public class Persona = new Persona(){
+  private string cedula;
+  private String nombre;
+  private short telefono();
+  private String direccion();
+  }
+public void CrearPersona (byte cedula, String nombre, short telefono, String direccion);
+modificarpersona (string cedula, String nombre, short telefono, String direccion)
+borrar persona (string cedula, String nombre, short telefono, String direccion)
+listar persona (string cedula, String nombre, short telefono, String direccion)
+  
+  
+  public void modificarpersona (string cedula, String nombre, short telefono, String direccion);
+   system.out.print("creando persona");
+  
+  public void borrarpersona (string cedula, String nombre, short telefono, String direccion);
+  system.out.println("borrando persona");
